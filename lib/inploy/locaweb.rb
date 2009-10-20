3a70a59930848c2838689aa213e217a166ea1858
Diego Carrion <dc.rec1@gmail.com>
2009-10-20 21:22:20 -0200

    removed depth -1 from locaweb template, as shallow repositories dont let others repositories push to them

--- lib/inploy/locaweb.rb
+++ lib/inploy/locaweb.rb
@@ -1,23 +1,23 @@
 module Inploy
   module Locaweb
     def remote_setup
-      run "rm -Rf #{tmp_path} && git clone --depth 1 . #{tmp_path} && tar czf - #{tmp_path} | ssh #{user}@#{host} 'tar xzfv - -C ~/ && mv ~#{tmp_path} #{path}/ && cd #{application_path} && rake inploy:local:setup'"
+      run "rm -Rf #{tmp_path} && git clone . #{tmp_path} && tar czf - #{tmp_path} | ssh #{user}@#{host} 'tar xzfv - -C ~/ && mv ~#{tmp_path} #{path}/ && cd #{application_path} && rake inploy:local:setup'"
     end
 
     def remote_update
       run "git push ssh://[#{user}@#{host}]#{application_path} master"
       super
     end
-    
+
     def local_setup
       super
       run "ln -s #{application_path}/public /home/#{user}/public_html/#{application}"
     end
-    
+
     def local_update
       after_update_code
     end
-    
+
     def path
       @path ||= "/home/#{user}/rails_app"
     end
--- spec/locaweb_spec.rb
+++ spec/locaweb_spec.rb
@@ -1,9 +1,9 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe Inploy::Deploy do
-  
+
   def expect_setup_in(path)
-    expect_command "rm -Rf /tmp/#{@application} && git clone --depth 1 . /tmp/#{@application} && tar czf - /tmp/#{@application} | ssh #{@user}@#{@host} 'tar xzfv - -C ~/ && mv ~/tmp/#{@application} #{path}/ && cd #{path}/#{@application} && rake inploy:local:setup'"
+    expect_command "rm -Rf /tmp/#{@application} && git clone . /tmp/#{@application} && tar czf - /tmp/#{@application} | ssh #{@user}@#{@host} 'tar xzfv - -C ~/ && mv ~/tmp/#{@application} #{path}/ && cd #{path}/#{@application} && rake inploy:local:setup'"
   end
 
   context "with template locaweb" do
@@ -21,7 +21,7 @@ describe Inploy::Deploy do
         expect_setup_in "/home/#{@user}/rails_app"
         subject.remote_setup
       end
-      
+
       it "should accept a custom path" do
         subject.path = path = '/home/xpto/apps'
         expect_setup_in path
@@ -34,19 +34,19 @@ describe Inploy::Deploy do
         expect_command "git push ssh://[#{@user}@#{@host}]/home/#{@user}/rails_app/#{@application} master"
         subject.remote_update
       end
-      
+
       it_should_behave_like "remote update"
     end
-    
+
     context "on local setup" do
       it "should create symbolic link" do
         expect_command "ln -s /home/#{@user}/rails_app/#{@application}/public /home/#{@user}/public_html/#{@application}"
         subject.local_setup
       end
-      
+
       it_should_behave_like "local setup"
     end
-    
+
     context "on local update" do
       it_should_behave_like "local update"
     end
