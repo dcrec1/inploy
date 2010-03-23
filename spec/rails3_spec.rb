84965a3e16d9707f6593be24162fe838be973f74
Felipe <felipe@felipe-laptop.(none)>
2010-03-23 15:46:24 -0300

    bundle install on remote setup for rails3 must be called before the rake tasks

--- spec/rails3_spec.rb
+++ spec/rails3_spec.rb
@@ -2,6 +2,15 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe Inploy::Deploy do
 
+  def expect_setup_with(branch, environment = 'production')
+    if branch.eql? 'master'
+      checkout = ""
+    else
+      checkout = "&& $(git branch | grep -vq #{branch}) && git checkout -f -b #{branch} origin/#{branch}"
+    end
+    expect_command "ssh #{@ssh_opts} #{@user}@#{@host} 'cd #{@path} && git clone --depth 1 #{@repository} #{@application} && cd #{@application} #{checkout} && bundle install ~/.bundle && rake inploy:local:setup environment=#{environment}'"
+  end
+
   context "with template rails3" do
     before :each do
       subject.template = :rails3
@@ -9,8 +18,16 @@ describe Inploy::Deploy do
       subject.hosts = [@host = 'gothic']
       subject.application = @application = "robin"
       subject.branch = @branch = 'branch'
+      subject.environment = @environment = "production"
       stub_commands
       mute subject
+    end
+
+    context "on remote setup" do
+      it "should clone the repository with the application name, checkout the branch, install gems from bundler and execute local setup" do
+        expect_setup_with @branch, @environment
+        subject.remote_setup
+      end
     end
 
     context "on install gems" do
