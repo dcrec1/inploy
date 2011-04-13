6582312b98081d5929e3bb8ffe78f8d87c6b3782
Brad O'Connor <oconnor.brad@gmail.com>
2011-04-13 14:42:18 +1000

    Changed install.rb to use FileUtils for Ruby 1.9
    Signed-off-by: Diego Carrion <dc.rec1@gmail.com>

--- install.rb
+++ install.rb
@@ -1,4 +1,4 @@
-require 'ftools'
+require 'fileutils'
 src = File.join(File.dirname(__FILE__), "deploy.rb.sample")
 dest = File.join(File.dirname(__FILE__), "..", "..", "..", "config", "deploy.rb")
-File.copy src, dest unless File.exists?(dest)
+FileUtils.copy src, dest unless File.exists?(dest)
