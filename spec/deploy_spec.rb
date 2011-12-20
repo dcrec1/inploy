29948635008f28d3db40f60968b67ae666c3161e
Sven Schwyn <schwyn@bitcetera.com>
2011-12-20 15:37:19 +0100

    Fix incorrect spec for after_git callback on remote reset

--- spec/deploy_spec.rb
+++ spec/deploy_spec.rb
@@ -210,7 +210,7 @@ describe Inploy::Deploy do
           rake "test_after_git"
         end
         expect_command("rake test_after_git").ordered
-        subject.update_code
+        subject.remote_reset :to => "fa3ed118970d8ddb0655be94b4c85d996c695476"
       end
     end
     
